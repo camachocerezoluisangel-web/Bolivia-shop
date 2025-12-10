33fc17b225c16416ce55e70636a95d2db2266b3c
camachocerezoluisangel-web <camachocerezoluisangel@gmail.com>
2025-12-10 01:05:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+const products = [
+  { id: 1, name: "Polera Bolivia", price: 80 },
+  { id: 2, name: "Gorra Santa Cruz", price: 50 },
+  { id: 3, name: "Taza personalizada", price: 35 }
+];
+
+const productList = document.getElementById("product-list");
+
+function renderProducts() {
+  products.forEach(product => {
+    const div = document.createElement("div");
+    div.className = "product";
+    div.innerHTML = `
+      <h3>${product.name}</h3>
+      <p>${product.price} Bs</p>
+      <button data-id="${product.id}">Agregar al carrito</button>
+    `;
+    productList.appendChild(div);
+  });
+}
+
+renderProducts();
 
